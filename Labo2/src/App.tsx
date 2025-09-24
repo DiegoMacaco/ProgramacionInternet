4f4f6bfd7689d15cb2360b4804d93d9234196d48
DiegoMacaco <diego.guisbert.621032@usalesiana.edu.bo>
2025-09-23 22:28:35 -0400

    feat: actualizar App.tsx e index.css con modo oscuro

--- Labo2/src/App.tsx
+++ Labo2/src/App.tsx
@@ -1,28 +1,44 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState<number>(0);
+  const [color, setColor] = useState<boolean>(false);
 
   const incrementar = () => {
-    if (count < 50) {
-      setCount(count + 5);
-    }
+    if (count < 50) setCount(count + 5);
   };
 
   return (
-    <div>
-      <h3>Contador de limites</h3>
-      <p style={{ fontSize: "20px" }}>Valor: {count} </p>
-      <button onClick={incrementar}>Incrementar</button>
-      <button disabled={count <= 0} onClick={() => setCount(count - 5)}>
-        {count <= 0 ? "Bloqueado" : "Decrementar"}
-      </button>
-      <p style={count < 50 ? { color: "blue" } : { color: "red" }}>
-        {count < 50 ? "No llegó al límite" : "Llegó al límite"}
-      </p>
-    </div>
+    <>
+
+      <div>
+        <h3>Contador de límites</h3>
+        <p style={{ fontSize: "20px" }}>Valor: {count} </p>
+        <button onClick={incrementar}>Incrementar</button>
+        <button disabled={count <= 0} onClick={() => setCount(count - 5)}>
+          {count <= 0 ? "Bloqueado" : "Decrementar"}
+        </button>
+
+        <p style={count < 50 ? { color: "blue" } : { color: "red" }}>
+          {count < 50 ? "No llegó al límite" : "Llegó al límite"}
+        </p>
+      </div>
+      <div
+        style={
+          !color
+            ? { backgroundColor: "#fff", color: "#111", padding: "10px", marginTop: "20px" }
+            : { backgroundColor: "#111", color: "#fff", padding: "10px", marginTop: "20px" }
+        }
+      >
+        <h3>{!color ? "Modo claro" : "Modo oscuro"}</h3>
+        <p>Este bloque cambia sus estilos usando un booleano en el estado</p>
+        <button onClick={() => setColor(!color)}>
+          Cambiar a {color ? "Modo claro" : "Modo oscuro"}
+        </button>
+      </div>
+    </>
   );
 }
 
-export default App
+export default App;
